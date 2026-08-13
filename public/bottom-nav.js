1fd26d0efe30adf9a1ba9371879d5bc4cddeff0e
valshkolin-create <val.shkolin@gmail.com>
2026-08-13 13:42:37 +0300

    Update bottom-nav.js

--- public/bottom-nav.js
+++ public/bottom-nav.js
@@ -165,7 +165,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <a href="/events" class="nav-item" data-path="events">
                 <i class="fa-solid fa-fire"></i><span>Гринд</span>
             </a>
-            <a href="event_page.html" class="nav-item" data-path="event_page.html" id="nav-games-btn">
+            <a href="public/event_page.html" class="nav-item" data-path="public/event_page.html" id="nav-games-btn">
                 <div class="comic-badge">ИГРА<br>НАЧАЛАСЬ</div>
                 <i class="fa-solid fa-gamepad"></i><span>Игры</span>
             </a>
