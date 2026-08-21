8a256bc0cc89533af51e7dffad143254e83a6c7c
valshkolin-create <val.shkolin@gmail.com>
2026-08-21 11:55:46 +0300

    Update menu.js

--- public/menu.js
+++ public/menu.js
@@ -4296,6 +4296,7 @@ window.openSwapModal = async () => {
             item.image_url && item.image_url.startsWith('http') &&
             item.source !== 'raffle' && // 🚫 Запрещаем скины с розыгрышей
             item.source !== 'auction'   // 🚫 Запрещаем скины с аукционов
+            !exp.isExpired;              // 🚫 ЗАПРЕТ ПРОСРОЧЕННЫХ
         );
         
         renderSwapInventory(availableItems);
